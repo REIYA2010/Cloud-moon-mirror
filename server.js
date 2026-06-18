7fc028293d9888c707169ca6a4c8538b40fe4a95
ねむ <myproxy0108@gmail.com>
2026-06-18 12:22:34 +0900

    Update server.js

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const app = express();
 
 // 1. あなたのWorker URLをここに入れる
 const CF_WORKER_URLS = [
-    "https://api-mangarw.72016.workers.dev/",
+    "https://api-nemu.myproxy0108.workers.dev",
     "https://mangarw-api.72016.workers.dev"
 ];
 
