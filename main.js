4dd1a8cbb98dffbfba2eb99590050da15642f5df
ねむ <myproxy0108@gmail.com>
2026-06-20 09:19:32 +0900

    Update main.js

--- main.js
+++ main.js
@@ -157,4 +157,4 @@ app.all('*', async (req, res) => {
 });
 
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => console.log(`--- ULTIMATE PROXY ENGINE ONLINE ---`));
+module.exports = app;
